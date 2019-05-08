a9114520b54ac8908bcf3ee6b9492ed76b21fcfb
Peter Mok <petermok97@gmail.com>
2019-05-08 16:14:58 +0800

    Update 2.sql

--- sql/2.sql
+++ sql/2.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE  IF NOT EXISTS `sch_challenge` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */;
+CREATE DATABASE  IF NOT EXISTS `sch_challenge` ;
 USE `sch_challenge`;
 -- MySQL dump 10.13  Distrib 8.0.13, for macos10.14 (x86_64)
 --
