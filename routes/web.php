6e9abc0c44fdb9d611d8311807152d75c78135e9
Peter Mok <petermok97@gmail.com>
2019-05-08 16:42:17 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -22,8 +22,6 @@ Route::get('/driver', "DriverController@getDriver")->name('driver');
 Route::get('/driver/delete', "DriverController@deleteDriver")->name('delete_driver');
 
 Route::get('/php_info', function() {
-   return response()->json([
-    'stuff' => phpinfo()
-   ]);
+   return phpinfo();
 })
 // Route::get('/driver/register', "DriverController@registerDriver");
